a9dab3d7b5a1902ef677a1ecab64e2cd4b5e8bf2
carloscacho <carlosemilio.ac@gmail.com>
2020-11-24 11:21:59 -0400

    share imagens

--- script.js
+++ script.js
@@ -7,3 +7,15 @@
     }
   }
 }
+
+function shareImage(titulo, texto, url){
+  if (navigator.share) {
+    navigator.share({
+      title: titulo,
+      text: texto,
+      url: url,
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  }
+}
